4ccaa11b44db25427d01f0f9811c37fc0234fc28
ankur0904 <ankursingh91002@gmail.com>
2023-08-08 23:38:33 +0530

    make updates in client

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,76 +1,46 @@
-import React from "react";
+import React from 'react';
 
-function Hero() {
+const Hero = () => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const formData = new FormData(form);
+
+        try {
+            const response = await fetch('http://localhost:8080/uploadProfilePicture', {
+                method: 'POST',
+                body: formData,
+            });
+
+            const data = await response.json();
+            console.log('Form data submitted successfully:', data);
+        } catch (error) {
+            console.error('Error submitting form data:', error);
+        }
+    };
+
     return (
-        <div class="container">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <div class="col">
-                    <div class="card shadow-sm mx-4 my-4" style={{ borderRadius: '60px' }}>
-                        <div class="card-body d-flex flex-column justify-content-between align-items-center ">
-                            <h5 className="hero-card"><span className="hero-card-span">Senior Data Analyst:</span> Analyze complex data sets to uncover insights and drive informed decision-making.</h5>
-                            <button type="button" class="btn btn-primary rounded-pill">Go</button>
-                        </div>
-                    </div>
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label">Description</label>
+                    <input type="text" className="form-control" id="description" name="description" aria-describedby="descriptionHelp" />
+                    <div id="descriptionHelp" className="form-text">Please provide the description of the job</div>
                 </div>
-                <div class="col">
-                    <div class="card shadow-sm mx-4 my-4" style={{ borderRadius: '60px' }}>
-                        <div class="card-body d-flex flex-column justify-content-between align-items-center">
-                            <h5 class="hero-card"><span className="hero-card-span">UX/UI Designer:</span> Create intuitive and visually appealing user experiences that enhance product usability.</h5>
-                            <button type="button" class="btn btn-primary rounded-pill">Go</button>
-                        </div>
-                    </div>
+                <div className="mb-3">
+                    <label htmlFor="resume" className="form-label">Resume/CV</label>
+                    <input type="file" accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" className="form-control" id="resume" name="resume" multiple />
                 </div>
-                <div class="col">
-                    <div class="card shadow-sm mx-4 my-4" style={{ borderRadius: '60px' }}>
-                        <div class="card-body d-flex flex-column justify-content-between align-items-center">
-                            <h5 class="hero-card"><span className="hero-card-span">Frontend Developer:</span> Build responsive and interactive web interfaces to deliver seamless user interactions.</h5>
-                            <button type="button" class="btn btn-primary rounded-pill">Go</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm mx-4 my-4" style={{ borderRadius: '60px' }}>
-                        <div class="card-body d-flex flex-column justify-content-between align-items-center ">
-                            <h5 className="hero-card"><span className="hero-card-span">Marketing Manager:</span> Develop and execute strategic marketing campaigns to promote brand visibility and engagement.</h5>
-                            <button type="button" class="btn btn-primary rounded-pill">Go</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm mx-4 my-4" style={{ borderRadius: '60px' }}>
-                        <div class="card-body d-flex flex-column justify-content-between align-items-center ">
-                            <h5 className="hero-card"><span className="hero-card-span">Software Engineer: </span> Design, develop, and maintain innovative software solutions to address complex challenges.</h5>
-                            <button type="button" class="btn btn-primary rounded-pill">Go</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm mx-4 my-4" style={{ borderRadius: '60px' }}>
-                        <div class="card-body d-flex flex-column justify-content-between align-items-center ">
-                            <h5 className="hero-card"><span className="hero-card-span">Senior Data Analyst:</span> Analyze complex data sets to uncover insights and drive informed decision-making.</h5>
-                            <button type="button" class="btn btn-primary rounded-pill">Go</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm mx-4 my-4" style={{ borderRadius: '60px' }}>
-                        <div class="card-body d-flex flex-column justify-content-between align-items-center ">
-                            <h5 className="hero-card"><span className="hero-card-span">Senior Data Analyst:</span> Analyze complex data sets to uncover insights and drive informed decision-making.</h5>
-                            <button type="button" class="btn btn-primary rounded-pill">Go</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm mx-4 my-4" style={{ borderRadius: '60px' }}>
-                        <div class="card-body d-flex flex-column justify-content-between align-items-center ">
-                            <h5 className="hero-card"><span className="hero-card-span">Senior Data Analyst:</span> Analyze complex data sets to uncover insights and drive informed decision-making.</h5>
-                            <button type="button" class="btn btn-primary rounded-pill">Go</button>
-                        </div>
-                    </div>
-                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-success hero-button-success rounded-circle"><svg xmlns="http://www.w3.org/2000/svg" height="75" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
+                    <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z" />
+                    <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z" />
+                </svg></button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Hero;
